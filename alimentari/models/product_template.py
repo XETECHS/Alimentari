5db6b4a1c4ea4b635f2e03360ab550a3ac3fee65
Jhayr Marin <jhayr.marin95@gmail.com>
2022-03-30 10:49:22 -0600

    [Ref] Función para actualizar impuestos y cambiar unidad de medida.

--- alimentari/models/product_template.py
+++ alimentari/models/product_template.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from dbm.ndbm import library
 from itertools import groupby
 from datetime import datetime, timedelta
+from math import prod
 
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError
@@ -37,6 +39,47 @@ class ProductTemplate(models.Model):
             else:
                 product.unit_to_box=0
                 product.complemento=0
+    
+    def asignar_tax_y_uom(self):
+        #Esta función asignará el impuesto y la unidad de medida a los productos 
+        #que no tienen una unidad de medida asignada correctamente y no tienen impuesto
+        cont=0
+        product_emplate=self.env['product.template'].search([])
+        iva_por_pagar= self.env.ref('l10n_gt.1_impuestos_plantilla_iva_por_pagar')
+        bebidas_alcoholicas= self.env.ref('__export__.account_tax_3_0da8763b')
+        unidad=self.env.ref('uom.product_uom_unit')
+        libra=self.env.ref('uom.product_uom_lb')
+
+        con_movimiento=[]
+        exitosos=0
+        error=0
+        for product in product_emplate:
+            impuestos=[]
+            cont+=1
+            for tax in product.taxes_id:
+                if tax.id not in impuestos:
+                    if tax.id==bebidas_alcoholicas.id:
+                        print("if",tax.id,bebidas_alcoholicas.id)
+                        impuestos.append(tax.id)
+                    elif tax.id==iva_por_pagar.id:
+                        impuestos.append(tax.id)
+                        print("elif",tax.id,iva_por_pagar.id)
+
+            if impuestos==[]:
+                impuestos.append(iva_por_pagar.id)
+
+            try:
+                exitosos+=1
+                product.taxes_id=impuestos
+                if product.uom_id != libra:
+                    product.uom_id=unidad.id
+            except:
+                error+=1
+                if product.id not in con_movimiento:
+                    con_movimiento.append(product.id)
+        
+        print("exitosos: ",exitosos," error: ",error)
+        print("movimientos: ",con_movimiento)
 
 
 
